ff4eb0458dbdaaf32a87e5a8815e2403df80d5ac
Sabbir <sabbir106706@gmail.com>
2020-05-16 01:00:20 +0600

    p2 using cpp

---
+++ Solving for Carrots/Solving for Carrots.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int n,p;
+    string s;
+    cin>>n>>p;
+    while(n--)
+    {
+        cin>>s;
+    }
+    cout<<p;
+    return 0;
+}
+
+
