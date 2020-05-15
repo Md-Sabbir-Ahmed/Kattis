2e64ab52b80362767822372bfcf447f16145cef9
Sabbir <sabbir106706@gmail.com>
2020-05-16 00:53:36 +0600

    p1 using cpp

---
+++ Stuck In A Time Loop/Stuck In A Time Loop.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        cout<<i<<" Abracadabra"<<"\n";
+    }
+
+    return 0;
+}
+
+
