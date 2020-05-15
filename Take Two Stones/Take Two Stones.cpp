d55d9c615877db2d2f440806597b8d4a5b62a31e
Sabbir <sabbir106706@gmail.com>
2020-05-16 01:58:20 +0600

    p4 using cpp

---
+++ Take Two Stones/Take Two Stones.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin>>n;
+    if(n%2==0)
+    {
+        cout<<"Bob";
+    }
+    else{
+        cout<<"Alice";
+    }
+
+    return 0;
+}
+
+
