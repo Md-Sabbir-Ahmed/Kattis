901f7789a5c4958a9540433a637679ea17d81305
Sabbir <sabbir106706@gmail.com>
2020-05-16 14:23:24 +0600

    p8 using cpp

---
+++ R2/R2.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int r1,r2,s;
+    cin>>r1>>s;
+    r2=(2*s)-r1;
+    cout<<r2<<"\n";
+
+    return 0;
+}
+
+
